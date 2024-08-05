6ee6723dcdf80786324de4c675e3731dd42cc768
mikid <Mikidov2510@>
2024-08-05 23:06:46 +0300

    1000 particles = ~500fps

--- Simulation.cpp
+++ Simulation.cpp
@@ -12,7 +12,7 @@ Simulation::Simulation(GLFWwindow* window, float fixedTimeStep)
 
     
 
-    for (int i = 0; i < 50; i++)
+    for (int i = 0; i < 1000; i++)
     {
         RigidBody body(10, Helper::getRandVector(), Helper::getRandVector());
 		physicsEngine.addBody(body);
@@ -48,7 +48,6 @@ void Simulation::run() {
             accumulatedTime -= fixedTimeStep;
 
         }
-
         // Render the scene
         renderer.render(physicsEngine.bodies);
 
